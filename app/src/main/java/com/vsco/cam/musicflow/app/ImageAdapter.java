72f82add1415efef2e36428d2778478648201a2a
Whitney Imura <whitneyimura@gmail.com>
2014-04-03 09:32:15 -0700

    Created a custom image adaptor to hook onto the main activity where i'm showing a grid view of images.  Currently, the images are hard coded into the app.

---
+++ app/src/main/java/com/vsco/cam/musicflow/app/ImageAdapter.java
@@ -0,0 +1,54 @@
+package com.vsco.cam.musicflow.app;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, 300));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(5, 5, 5, 5);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.avicii_1, R.drawable.avicii_2,
+            R.drawable.calvin_harris, R.drawable.ellie_goulding1,
+            R.drawable.ellie_goulding2, R.drawable.karmin,
+            R.drawable.skrillex_1, R.drawable.deadmou5,
+            R.drawable.swedish_house_mafia, R.drawable.tim_berg,
+            R.drawable.tommy_trash, R.drawable.zedd
+    };
+}
