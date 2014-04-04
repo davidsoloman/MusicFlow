fbc2ab5cc7beb84eabf8008a56f4cc2f7a68a305
Whitney Imura <whitneyimura@gmail.com>
2014-04-04 15:09:09 -0700

    added jackson libs and added json api adapter

---
+++ app/src/main/java/com/musicflow/app/CommonMapper.java
@@ -0,0 +1,20 @@
+package com.musicflow.app;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musicflow.app.BaseJson;
+
+public abstract class CommonMapper {
+    protected ObjectMapper objMapper;
+    protected JsonFactory jsonFactory;
+    protected JsonParser jsonParser;
+
+    public CommonMapper() {
+        objMapper = new ObjectMapper();
+        jsonFactory = new JsonFactory();
+        jsonParser = null;
+    }
+
+    public abstract BaseJson parseJson(String json);
+}
