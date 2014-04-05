28267abb441cbcdd1cb32ee84f613d20d024b381
Whitney Imura <whitneyimura@gmail.com>
2014-04-05 00:29:09 -0700

    renamed dir a long time ago. now removing from github

--- app/src/main/java/com/musicflow/app/BaseJson.java
+++
@@ -1,7 +0,0 @@
-package com.musicflow.app;
-
-import java.io.Serializable;
-
-public abstract class BaseJson implements Serializable {
-    public abstract void fillIn(BaseJson parseJson) throws Exception;
-}
--- app/src/main/java/com/musicflow/app/json/data/BaseJson.java
+++
@@ -1,7 +0,0 @@
-package com.musicflow.app.json.data;
-
-import java.io.Serializable;
-
-public abstract class BaseJson implements Serializable {
-    public abstract void fillIn(BaseJson parseJson) throws Exception;
-}
