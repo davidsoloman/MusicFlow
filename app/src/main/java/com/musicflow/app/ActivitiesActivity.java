24b17bcc0c47bc580dceb211269ccc82e80030b5
Whitney Imura <whitneyimura@gmail.com>
2014-04-26 21:20:05 -0700

    set the correct title for activities

--- app/src/main/java/com/musicflow/app/ActivitiesActivity.java
+++ app/src/main/java/com/musicflow/app/ActivitiesActivity.java
@@ -3,9 +3,6 @@ package com.musicflow.app;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 
-/**
- * Created by whitney on 4/26/14.
- */
 public class ActivitiesActivity extends BeatsMusicActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +12,8 @@ public class ActivitiesActivity extends BeatsMusicActivity {
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(sectionsPagerAdapter);
 
-        drawerList.setItemChecked(0, true);
-        setTitle(navTiles[0]);
+        drawerList.setItemChecked(3, true);
+        setTitle(navTiles[3]);
         drawerLayout.closeDrawer(drawerList);
     }
 }
