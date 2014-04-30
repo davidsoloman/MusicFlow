de56ad2fc2c9f6c1fa78864bacf63595e945a699
Whitney Imura <whitneyimura@gmail.com>
2014-04-29 22:42:22 -0700

    Reformat list view for artists

--- app/src/main/java/com/musicflow/app/ArtistAdapter.java
+++ app/src/main/java/com/musicflow/app/ArtistAdapter.java
@@ -42,7 +42,7 @@ public class ArtistAdapter extends ArrayAdapter<Artist> {
 
         ImageView artistProfileImage = (ImageView) rowView.findViewById(R.id.artist_profile_image);
         String url = "https://partner.api.beatsmusic.com/v1/api/artists/" + current.getId() + "/images/default?client_id=frksnm8edw2t8ddebhkqkjwk&size=medium";
-        Picasso.with(context).load(url).placeholder(R.drawable.placeholder).into(artistProfileImage);
+        Picasso.with(context).load(url).placeholder(R.drawable.placeholder).fit().centerCrop().into(artistProfileImage);
 
         TextView artistName = (TextView) rowView.findViewById(R.id.artist_name);
         artistName.setText(current.getName());
