26908175380d511471dec90b39bd8ef2c3bb694b
Whitney Imura <whitneyimura@gmail.com>
2014-05-01 11:18:55 -0700

    Updated URL factory for search

--- app/src/main/java/com/musicflow/app/ActivitiesSearchFragment.java
+++ app/src/main/java/com/musicflow/app/ActivitiesSearchFragment.java
@@ -1,27 +1,29 @@
 package com.musicflow.app;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ActivitiesSearchFragment extends BeatsMusicFragment {
+import com.musicflow.app.utility.BaseSearchFragment;
+
+public class ActivitiesSearchFragment extends BaseSearchFragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    public static ActivitiesSearchFragment newInstance(int sectionNumber) {
-        ActivitiesSearchFragment fragment = new ActivitiesSearchFragment();
+    public static AlbumsSearchFragment newInstance(int sectionNumber) {
+        AlbumsSearchFragment fragment = new AlbumsSearchFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_SECTION_NUMBER, sectionNumber);
         fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
+    public String getNetworkUrl() {
+        return "https://partner.api.beatsmusic.com/v1/api/search/predictive?q=" + Uri.encode(searchText.getText().toString()) + "&client_id=frksnm8edw2t8ddebhkqkjwk";
+    }
 
-        View rootView = inflater.inflate(R.layout.activity_search, container, false);
-        innerFrame.addView(rootView);
-        return innerFrame;
+    public static CharSequence getTitle() {
+        return "Search";
     }
 }
--- app/src/main/java/com/musicflow/app/AlbumsSearchFragment.java
+++ app/src/main/java/com/musicflow/app/AlbumsSearchFragment.java
@@ -3,6 +3,7 @@ package com.musicflow.app;
 import android.net.Uri;
 import android.os.Bundle;
 
+import com.musicflow.app.network.UrlFactory;
 import com.musicflow.app.utility.BaseSearchFragment;
 
 public class AlbumsSearchFragment extends BaseSearchFragment {
@@ -17,7 +18,7 @@ public class AlbumsSearchFragment extends BaseSearchFragment {
     }
 
     public String getNetworkUrl() {
-        return "https://partner.api.beatsmusic.com/v1/api/search/predictive?q=" + Uri.encode(searchText.getText().toString()) + "&client_id=frksnm8edw2t8ddebhkqkjwk";
+        return UrlFactory.search(searchText.getText().toString());
     }
 
     public static CharSequence getTitle() {
