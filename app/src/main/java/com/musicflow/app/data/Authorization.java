819315ef86ce520ecd852266f56c039cf6c270ab
Dan Sabin <dsabin@beatsmusic.com>
2014-05-01 21:02:49 -0700

    finished building out auth requests/responses

---
+++ app/src/main/java/com/musicflow/app/data/Authorization.java
@@ -0,0 +1,7 @@
+package com.musicflow.app.data;
+
+/**
+ * Created by dsabin on 5/1/14.
+ */
+public class Authorization {
+}
---
+++ app/src/main/java/com/musicflow/app/mappers/AuthorizationMapper.java
@@ -0,0 +1,29 @@
+package com.musicflow.app.mappers;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.musicflow.app.data.Activities;
+import com.musicflow.app.data.BaseJson;
+
+import java.io.IOException;
+
+public class AuthorizationMapper extends CommonMapper {
+    
+    @Override
+    public BaseJson parseJson(String json) {
+        Activities activities = new Activities();
+
+        try {
+            jsonParser = jsonFactory.createParser(json);
+            activities = objMapper.readValue(jsonParser, Activities.class);
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return activities;
+    }
+}
