01809ccf2c4ad2d7c94b3bc1aaffad85685eed68
Dan Sabin <dsabin@beatsmusic.com>
2014-05-01 18:22:26 -0700

    login activity shows toast and saves value

--- app/src/main/java/com/musicflow/app/login/LoginActivity.java
+++ app/src/main/java/com/musicflow/app/login/LoginActivity.java
@@ -1,11 +1,8 @@
 package com.musicflow.app.login;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
