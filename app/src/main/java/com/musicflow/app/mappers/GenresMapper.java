1998d9767d5fa9031d87fc5a59643ec7e6d5d097
Whitney Imura <whitneyimura@gmail.com>
2014-05-03 19:32:10 -0700

    Genres

---
+++ app/src/main/java/com/musicflow/app/mappers/GenresMapper.java
@@ -0,0 +1,33 @@
+package com.musicflow.app.mappers;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.musicflow.app.data.BaseJson;
+import com.musicflow.app.data.GenreWrapper;
+import com.musicflow.app.data.Genres;
+
+/**
+ * Maps the list of genres.
+ */
+public class GenresMapper extends CommonMapper {
+
+    @Override
+    public BaseJson parseJson(String json) {
+        Genres genres = new Genres();
+
+        try {
+            jsonParser = jsonFactory.createParser(json);
+            genres = objMapper.readValue(jsonParser, Genres.class);
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return genres;
+    }
+}
